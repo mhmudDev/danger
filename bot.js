72907b0bc4fb84372315fcc7295344e74bddb22f
mhmudDev <44923654+mhmudDev@users.noreply.github.com>
2019-06-06 17:11:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,10 @@ const client = new Discord.Client();
     bot = new Discord.Client({sisableEveryone: true})
     console.log("muuuuuuuuute,");
 bot.on('guildMemberAdd', member => {
-const pirse= member.guild.channels.get("إيدي الروم حق السيرفر الي ترحب فيه");
+const pirse= member.guild.channels.get("583623939503292426");
 if(!pirse) return;
 if(pirse) {
-setTimeout(() => pirse.send(`رسالة الولكمة هنآ`), 4000)        
+setTimeout(() => pirse.send(`**Welcome To Darkside** :cloud: `), 4000)        
 }
 });
 
